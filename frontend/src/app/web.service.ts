3c7839cb8beb67eb8a5ec9e9b0b3a64b7f21d833
Rafael Dreher <dreher@me.com>
2018-02-27 20:30:26 -0800

    Adds error handling functions to post and get methods

--- frontend/src/app/web.service.ts
+++ frontend/src/app/web.service.ts
@@ -1,5 +1,7 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
@@ -7,16 +9,33 @@ export class WebService {
   BASE_URL = 'http://localhost:5000/api';
   public messages = [];
 
-  constructor(private httpClient: Http) {
+  constructor(private httpClient: Http, private sb: MatSnackBar) {
     this.getMessages();
   }
 
   async getMessages() {
-    const response = await this.httpClient.get(this.BASE_URL + '/messages').toPromise();
-    this.messages = response.json();
+
+    try {
+      const resp = await this.httpClient.get(this.BASE_URL + '/messages').toPromise();
+      this.messages = resp.json();
+    } catch (error) {
+        this.handleError('Unable to get messages');
+    }
+
   }
+
   async postMessage(message) {
+    try {
+      const resp = await this.httpClient.post(this.BASE_URL + '/messages', message).toPromise();
+      this.messages.push(resp.json());
+    } catch (error) {
+      this.handleError('Unable to post message');
+    }
     const response = await this.httpClient.post(this.BASE_URL + '/messages', message).toPromise();
     this.messages.push(response.json());
+  }
+
+  private handleError(error) {
+    this.sb.open(error, 'close', {duration: 2000});
   }
 }
