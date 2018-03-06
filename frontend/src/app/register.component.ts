6586d31f42caa5f0e29a36e5e6d24e6b88244743
Rafael Dreher <dreher@me.com>
2018-03-05 22:19:56 -0800

    Fix style

--- frontend/src/app/register.component.ts
+++ frontend/src/app/register.component.ts
@@ -44,6 +44,6 @@ function emailValid() {
   return control => {
     // tslint:disable-next-line:max-line-length
     const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regex.test(control.value) ? null : { invalidEmai: true};
+    return regex.test(control.value) ? null : { invalidEmail: true};
   };
 }
