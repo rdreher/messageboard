8597f36de80d32a8c0375229756a8e16cb9514f6
Rafael Dreher <dreher@me.com>
2018-03-07 00:48:27 -0800

    fixes

--- MessageBoardBackend/MessageBoardBackend/Controllers/MessagesController.cs
+++ MessageBoardBackend/MessageBoardBackend/Controllers/MessagesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
