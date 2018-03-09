4abd45de64dbf6611512e172d20c1d4d0e0ab675
Rafael Dreher <dreher@me.com>
2018-03-08 20:52:40 -0800

    Rename project

---
+++ MessageBoardBackend/MessageBoardAPI/ApiContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MessageBoardAPI
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : 
+            base(options) { }
+
+        public DbSet<Models.Message> Messages { get; set; }
+    }
+}
---
+++ MessageBoardBackend/MessageBoardAPI/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessageBoardAPI.Models
+{
+    public class Message
+    {
+        public string Id { get; set; }
+        public string Owner { get; set; }
+        public string Text { get; set; }
+    }
+}
