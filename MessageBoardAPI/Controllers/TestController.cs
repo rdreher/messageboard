1df26d66a93804c1a5254fdea93253dc0f7fdd17
Rafael Dreher <dreher@me.com>
2018-03-22 20:21:53 -0700

    Test Controller

--- MessageBoardAPI/Controllers/TestController.cs
+++ MessageBoardAPI/Controllers/TestController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.IO;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,27 +22,29 @@ namespace MessageBoardAPI.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public string Get([FromQuery]string id)
         {
-            return "value";
+            //return id;
+            return Test(id).Result.ToString();
+                
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
+        protected async Task<String> Test(string id) {
+            //var uri = new Uri("http://localhost:5000/api/test/1?id=" + id);
+            var uri = new Uri(id);
+            HttpClient client = new HttpClient();
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+            var response = await client.GetAsync(uri.ToString());
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            if (response.StatusCode == System.Net.HttpStatusCode.OK) {
+                var stream = await response.Content.ReadAsStreamAsync();
+                string content;
+                using (var readStream = new StreamReader(stream, System.Text.Encoding.UTF8)) {
+                    content = readStream.ReadToEnd();
+                }
+                return content;
+            }
+            return null;
         }
     }
 }
