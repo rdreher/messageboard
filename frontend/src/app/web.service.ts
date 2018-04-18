50af95ec2bf7bfd9badf5a7ca15d5a2b42235cc6
Rafael Dreher <dreher@me.com>
2018-04-18 12:40:30 -0700

    Adding external endpoint

--- frontend/src/app/web.service.ts
+++ frontend/src/app/web.service.ts
@@ -10,7 +10,7 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class WebService {
-  BASE_URL = 'https://messageboardapiv2.azurewebsites.net/api';
+  BASE_URL = 'https://apimessageboard.azurewebsites.net/api';
 
   private messageStore = [];
 
