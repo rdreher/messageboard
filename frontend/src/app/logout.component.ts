fe8a202d792afc6cb4b69dbfbf13da00a01e3dd1
Rafael Dreher <dreher@dreher-mbp.local>
2018-09-27 15:36:18 -0700

    Back on the old ID

--- frontend/src/app/logout.component.ts
+++ frontend/src/app/logout.component.ts
@@ -4,7 +4,7 @@ import { adal } from 'adal-angular';
 
 const config: adal.Config = {
   tenant: 'rdreher.com',
-  clientId: 'f101a8ad-4d6b-4f45-9736-51015e1a58e8'
+  clientId: 'fea922e0-f20b-4038-bc96-aa7be62c6e2e'
 };
 
 @Component({
